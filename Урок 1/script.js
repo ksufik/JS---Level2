c60e9e1738615160a4278c6baee90b22fbb124c0
Ksenia <naomi355@yandex.ru>
2021-07-25 15:52:52 +0300

    Сокращение функции

--- Урок 1/script.js
+++ Урок 1/script.js
@@ -5,11 +5,10 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title = 'ProductName', price = 0) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-};
+const renderGoodsItem = (title = 'ProductName', price = 0) => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 
-const renderGoodsList = (list) => {
+
+const renderGoodsList = list => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
     document.querySelector('.goods-list').innerHTML = goodsList; //- goodsList сначала приводится к строке, в массиве эл-ты перечислялись через запятую, и эта запятая тоже стала частью строки.
     //     let listShow = document.querySelector('.goods-list'); - так товары выводятся без запятой
